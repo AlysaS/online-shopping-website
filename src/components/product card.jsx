d253835810f7bd4ae8556b7b280063b6ce986c29
AS <aschecht3@student.touro.edu>
2023-04-27 22:37:15 -0400

    started working on product page

--- src/components/product card.jsx
+++ src/components/product card.jsx
@@ -1,34 +1,34 @@
 import { Card, CardContent, CardActions, CardMedia, Typography, Button, Rating, Box, Stack } from "@mui/material";
 import * as React from "react";
-import { useEffect, useState } from "react"; 
-import productImage from "../pictures/sampleProduct.jpg";
 
 
+export const ProductCard = (props) => {
 
-export function ProductCard(){
+  const {product} = props;
 
   return (
       
     <Card  sx={{ width: 275, height:350}}>
       <CardMedia
-        sx={{ height: 170, width:150}}
-        image="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+        sx={{ height: 170, width:150,display: "flex", justifyContent: "center", objectFit:"contain"}}
+        component="img"
+        image={product.image}
         title="product image"
 
       />
       <CardContent>
         <Typography gutterBottom variant="h6" component="div" sx={{fontWeight: "bold", textAlign:"center"}}>
-        Mens Casual Premium Slim Fit T-Shirts 
+        {product.title}
         </Typography >
         
-        <Stack direction="row" spacing= {1.5} sx={{justifyContent:"center"}}>
-            <Rating name="simple-controlled" value={4.1} size="small"  readOnly
+        <Stack direction="row" spacing= {1} sx={{justifyContent:"center"}}>
+            <Rating name="simple-controlled" value={product.rating.rate} size="small"  readOnly
             /** onChange={(event, newValue) => {setValue(newValue)}*/
             />
-            <Typography>({259})</Typography>
+            <Typography>({product.rating.count})</Typography>
         </Stack>
         <Typography variant="h6" sx={{textAlign:"center"}}>
-           $ 22.30
+          ${product.price}
         </Typography>
       </CardContent>
     </Card>
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,11 +1,52 @@
 import * as React from "react";
 import { ProductCard } from "./product card";
+import { Typography } from "@mui/material";
+
+import { useEffect, useState } from "react"; 
+
+
+
+
 
 export function Products(){
+
+    const [products, setProducts] = useState([]);
+
+
+
+    function getProducts(){
+            fetch(
+        "https://fakestoreapi.com/products/category/women's clothing"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Products: ");
+          console.log(data);
+
+        //   const products = data.map(dataItem => dataItem.product)
+        //   console.log("new pproduct")
+        //   console.log(products)
+  
+          setProducts(data);
+        });
+    }
+
+    useEffect(() => {
+        getProducts();
+      }, []);
+
+  
+
     return (
         <>
-            <h1>Products</h1>
-            <ProductCard></ProductCard>
+        <Typography gutterBottom variant="h3" component="div" sx={{fontWeight: "bold", textAlign:"center", marginBlock:5}}>
+           Products 
+        </Typography >
+
+        {products.map((product) => (
+              
+                 <ProductCard product={product}></ProductCard>
+            ))}
         </>
     
     );
