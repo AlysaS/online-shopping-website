7dad3fd95307fa79d8c8dcfe717a9237594fccb2
AS <aschecht3@student.touro.edu>
2023-05-17 15:32:02 -0400

    made some changes on the cart page

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Typography, Box, ListItem, List, Grid, Button} from "@mui/material";
+import { Typography, Box, ListItem, List, Card,Grid, Button, Stack} from "@mui/material";
 import { CartContext } from "../../state/cart/cart-context";
 import { useContext } from "react"; 
 import { CartItem } from "./cartItem";
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 import { OrdersContext } from "../../state/orders/orders-context";
 import { ordersActions } from "../../state/orders/orders-reducer";
 import { cartActions } from "../../state/cart/cart-reducer";
+import emptyCartImage from "../../pictures/emptyCartImage.png";
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 
 export function Cart(){
     
@@ -34,12 +36,17 @@ export function Cart(){
     
     return (
     <Box  sx={{paddingBottom:15}}>
+        
+        
+        
         <Box id="cartItems">
         <Typography gutterBottom variant="h3" component="div" sx={{fontWeight: "bold", textAlign:"center", marginBlock:5}}>
            Cart
         </Typography >
         
-
+        
+        { cartState.cart.length != 0 ? (
+        <Box>
         <List sx={{mx:"auto", alignItems:"center"}}>
             {cartState.cart.map((item) => (
                 <ListItem sx={{margin:"auto", alignItems:"center", width:900}}>
@@ -48,18 +55,31 @@ export function Cart(){
             ))}
         </List>
 
-        <Box>Total Price: {/* Put total price here -- add up qty*price of each item */}</Box>
+       <Box>Total Price: {/* Put total price here -- add up qty*price of each item*/ }</Box> 
 
 
         <Button size="large" variant="contained" 
         onClick={() => placeOrder()}
         sx={{my: 6, display:"flex", mx:"auto", fontSize:20 , fontWeight:"bold" }}>Place Order</Button>
-
         </Box>
-        <Box id="saved for later">
+        ) : (
+            <Card sx={{border:1, width:"50%", mx:"auto", py:5}}>
+            <Stack direction = "row" spacing={6} sx={{ display:"flex", justifyContent:"center", alignItems:"center" }}>
+                <Box component="img" src={emptyCartImage} alt="emptyCartImage" sx={{width:170}}/>
+                <Typography sx={{textAlign: "center", fontSize:30, fontWeight:"bold"}}>Your cart is empty</Typography>
+            </Stack>
+            </Card>
+            )}
+
+         </Box>
         <Typography gutterBottom variant="h3" component="div" sx={{fontWeight: "bold", textAlign:"center", marginBlock:5}}>
            Saved For Later
         </Typography >
+
+<Box id="saved for later">
+         { cartState.saveForLater.length != 0 ? (
+        <Box>
+        
 
         <List sx={{mx:"auto", alignItems:"center"}}>
             {cartState.saveForLater.map((item) => (
@@ -68,8 +88,16 @@ export function Cart(){
                 </ListItem>
             ))}
         </List>
-       
-        </Box>
+       </Box>
         
+         ) : (
+            <Card sx={{border:1, width:"50%", mx:"auto", py:5}}>
+            <Stack direction = "row" spacing={6} sx={{ display:"flex", justifyContent:"center", alignItems:"center" }}>
+                <StarBorderOutlinedIcon fontSize="large"/>
+                <Typography sx={{textAlign: "center", fontSize:30, fontWeight:"bold"}}>You do not have any saved items</Typography>
+            </Stack>
+            </Card>
+         )}
+    </Box>
     </Box>);
 }
